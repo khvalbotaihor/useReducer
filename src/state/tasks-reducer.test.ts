c4207cc357d6e7e47cce72ce149c95bb4a6f9ba7
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-17 16:37:08 +0300

    - moved tests pre-conditions data to a beforeEach section

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -32,20 +32,6 @@ test('correct task should be deleted from correct array', () => {
 });
 
 test('correct task should be added to correct array', () => {
-    const startState: TasksStateType = {
-        "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
-        ],
-        "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "2", title: "milk", isDone: true },
-            { id: "3", title: "tea", isDone: false }
-        ]
-    };
-
-
     const action = addTaskAC("juce", "todolistId2");
     const endState = tasksReducer(startState, action)
 
