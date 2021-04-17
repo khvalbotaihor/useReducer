1abb523ca334dbfa3bf4dbfb3fb86b68bce1c982
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-17 16:37:48 +0300

    - moved tests pre-conditions data to a beforeEach section

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -60,19 +60,6 @@ test('title of specified task should be changed', () => {
 });
 
 test('new property with new array should be added when new todolist is added', () => {
-    const startState: TasksStateType = {
-        "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
-        ],
-        "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "2", title: "milk", isDone: true },
-            { id: "3", title: "tea", isDone: false }
-        ]
-    };
-
     const action = addTodolistAC("title no matter");
     const endState = tasksReducer(startState, action)
 
