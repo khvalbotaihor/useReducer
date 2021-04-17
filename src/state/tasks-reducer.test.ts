886d59ca1a6b0c9f3fa23fb7a0503dae1a41c86a
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-17 16:37:36 +0300

    - moved tests pre-conditions data to a beforeEach section

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -52,19 +52,6 @@ test('status of specified task should be changed', () => {
 });
 
 test('title of specified task should be changed', () => {
-    const startState: TasksStateType = {
-        "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
-        ],
-        "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "2", title: "milk", isDone: true },
-            { id: "3", title: "tea", isDone: false }
-        ]
-    };
-
     const action = changeTaskTitleAC("2", "Milkyway", "todolistId2");
     const endState = tasksReducer(startState, action)
 
