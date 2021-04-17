2d92fa299ef8e7d32a1827e9e084ec7476035e38
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-17 16:35:06 +0300

    - moved tests pre-conditions data to a beforeEach section

--- src/state/todolists-reducer.test.tsx
+++ src/state/todolists-reducer.test.tsx
@@ -59,16 +59,7 @@ test('correct todolist should be added', () => {
 });
 
 test('correct todolist should change its name', () => {
-    let todolistId1 = v1();
-    let todolistId2 = v1();
-
     let newTodolistTitle = "New Todolist";
-
-    const startState: Array<TodolistType> = [
-        {id: todolistId1, title: "What to learn", filter: "all"},
-        {id: todolistId2, title: "What to buy", filter: "all"}
-    ]
-
 
     const action = changeTodolistTitleAC(todolistId2, newTodolistTitle);
 
