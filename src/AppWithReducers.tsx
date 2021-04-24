2011878156154059377b66e28b857ee765a99ea1
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-24 16:01:03 +0300

    - added useReducer instead useState

--- src/AppWithReducers.tsx
+++ src/AppWithReducers.tsx
@@ -23,12 +23,12 @@ function AppWithReducers() {
     let todolistId1 = v1();
     let todolistId2 = v1();
 
-    let [todolists, setTodolists] = useReducer(todolistsReducer, [
+    let [todolists, dispatchToTodolist] = useReducer(todolistsReducer, [
         {id: todolistId1, title: "What to learn", filter: "all"},
         {id: todolistId2, title: "What to buy", filter: "all"}
     ])
 
-    let [tasks, setTasks] = useReducer(tasksReducer, {
+    let [tasks, dispatchToTasks] = useReducer(tasksReducer, {
         [todolistId1]: [
             {id: v1(), title: "HTML&CSS", isDone: true},
             {id: v1(), title: "JS", isDone: true}
