28c575131f0c6996d6a5ebac4cd141a5c417ece8
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-24 16:13:24 +0300

    - refactor functions to use reducers

--- src/AppWithReducers.tsx
+++ src/AppWithReducers.tsx
@@ -6,7 +6,7 @@ import {AddItemForm} from './AddItemForm';
 import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from '@material-ui/core';
 import {Menu} from '@material-ui/icons';
 import {todolistsReducer} from "./state/todolists-reducer";
-import {removeTaskAC, tasksReducer} from "./state/tasks-reducer";
+import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer} from "./state/tasks-reducer";
 
 export type FilterValuesType = "all" | "active" | "completed";
 export type TodolistType = {
@@ -46,26 +46,13 @@ function AppWithReducers() {
     }
 
     function addTask(title: string, todolistId: string) {
-        let task = {id: v1(), title: title, isDone: false};
-        //достанем нужный массив по todolistId:
-        let todolistTasks = tasks[todolistId];
-        // перезапишем в этом объекте массив для нужного тудулиста копией, добавив в начало новую таску:
-        tasks[todolistId] = [task, ...todolistTasks];
-        // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-        setTasks({...tasks});
+        const action = addTaskAC(title, todolistId)
+        dispatchToTasksReducer(action)
     }
 
     function changeStatus(id: string, isDone: boolean, todolistId: string) {
-        //достанем нужный массив по todolistId:
-        let todolistTasks = tasks[todolistId];
-        // найдём нужную таску:
-        let task = todolistTasks.find(t => t.id === id);
-        //изменим таску, если она нашлась
-        if (task) {
-            task.isDone = isDone;
-            // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-            setTasks({...tasks});
-        }
+        const action = changeTaskStatusAC(id, isDone, todolistId)
+        dispatchToTasksReducer(action)
     }
 
     function changeTaskTitle(id: string, newTitle: string, todolistId: string) {
