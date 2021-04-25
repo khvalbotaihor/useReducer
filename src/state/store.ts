bf658e30b21a869a63fb847362f929455f551ad2
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-25 15:14:42 +0300

    - added store value

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,6 @@
 import {createStore} from "redux";
 import {tasksReducer} from "./tasks-reducer";
 
-const store = createStore();
+export const store = createStore();
+// @ts-ignore
+window.store = store;
