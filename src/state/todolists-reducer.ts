93254ff892fdd8bc5b8618852f4b1d97a594475d
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-25 15:37:04 +0300

    - added type for initial state

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -26,7 +26,7 @@ type ActionsType = RemoveTodolistActionType | AddTodolistActionType | ChangeTodo
 export let todolistId1 = v1();
 export let todolistId2 = v1();
 
-const initialState = [
+const initialState: Array<TodolistType> = [
     {id: todolistId1, title: "What to learn", filter: "all"},
     {id: todolistId2, title: "What to buy", filter: "all"}
 ]
