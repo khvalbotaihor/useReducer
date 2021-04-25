56cb68a5623c2939e8c01ccce0a7434dfdf721ff
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-25 15:18:27 +0300

    - added store value

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,17 @@
 import {combineReducers, createStore} from "redux";
 import {tasksReducer} from "./tasks-reducer";
 import {todolistsReducer} from "./todolists-reducer";
+import {TasksStateType, TodolistType} from "../AppWithRedux";
 
 const rootReducer = combineReducers({
     todolists: todolistsReducer,
     tasks : tasksReducer
 })
+
+type AppRootState = {
+    todolists: Array<TodolistType>
+    tasks : TasksStateType
+}
 
 export const store = createStore(rootReducer);
 // @ts-ignore
