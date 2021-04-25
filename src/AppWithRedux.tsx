9986feca6b7cd1cc32408d15cdd3f9d9ae07a1d2
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-25 15:44:48 +0300

    - updated state

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -33,7 +33,7 @@ function AppWithRedux() {
     let todolistId1 = v1();
     let todolistId2 = v1();
 
-    let [todolists, dispatchToTodolistReducer] = useReducer(todolistsReducer, [
+    let [todolists] = useReducer(todolistsReducer, [
         {id: todolistId1, title: "What to learn", filter: "all"},
         {id: todolistId2, title: "What to buy", filter: "all"}
     ])
